be89266fb2499f0810848cd6171a4f57514d57b7
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-09-11 01:37:30 -0300

    adiciona exercicio 2

--- Fundamentos/bloco-08-javascript-es6-high-order-functions/dia-02-es6-foreach-fint-some-every-sort/exercise1.js
+++ Fundamentos/bloco-08-javascript-es6-high-order-functions/dia-02-es6-foreach-fint-some-every-sort/exercise1.js
@@ -70,3 +70,17 @@ const authorBornIn1947 = () => {
 }
 
 assert.strictEqual(authorBornIn1947(), 'Stephen King');
+
+function smallerName() {
+    let nameBook;
+    // escreva aqui o seu código
+    books.forEach((book) => {
+        if (!nameBook || book.name.length < nameBook.length) {
+          nameBook = book.name;
+        }
+      });
+    // Variável nameBook que receberá o valor do menor nome;
+    return nameBook;
+  }
+  
+  assert.strictEqual(smallerName(), 'Duna');
