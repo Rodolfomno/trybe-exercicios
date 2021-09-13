27d89e31100ba95787acb4e57ff8318ed8a43abe
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-09-13 14:23:54 -0300

    adiciona exercicio

--- Fundamentos/bloco-08-javascript-es6-high-order-functions/dia-02-es6-foreach-fint-some-every-sort/exercise1.js
+++ Fundamentos/bloco-08-javascript-es6-high-order-functions/dia-02-es6-foreach-fint-some-every-sort/exercise1.js
@@ -73,14 +73,33 @@ assert.strictEqual(authorBornIn1947(), 'Stephen King');
 
 function smallerName() {
     let nameBook;
-    // escreva aqui o seu código
+
     books.forEach((book) => {
         if (!nameBook || book.name.length < nameBook.length) {
           nameBook = book.name;
         }
       });
-    // Variável nameBook que receberá o valor do menor nome;
+      
     return nameBook;
   }
   
   assert.strictEqual(smallerName(), 'Duna');
+
+  const expectedResult = {
+    author: {
+      birthYear: 1948,
+      name: 'George R. R. Martin',
+    },
+    genre: 'Fantasia',
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    releaseYear: 1991,
+  };
+  
+  function getNamedBook() {
+    return books.find((book) => book.name.length === 26);
+  }
+    
+assert.deepStrictEqual(getNamedBook(), expectedResult);
+
+  
